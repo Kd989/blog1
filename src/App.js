400339899ee5d9c6c9eaef0cbcf92e504c75621e
Kunal Das <kunal.d@finacussolutions.local>
2024-05-02 12:15:17 +0530

    oflinebootstrap

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import Nav from "./component/Nav"
 import { AllRoutes } from './route';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min";
 
 
 function App() {
